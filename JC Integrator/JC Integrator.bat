b58527efe5f9fb5479994de865e0d920cb370952
Jesse <canav021@umn.edu>
2024-10-11 16:28:43 -0500

    Fixed path in .bat file

--- JC Integrator/JC Integrator.bat
+++ JC Integrator/JC Integrator.bat
@@ -1,6 +1,6 @@
 @echo off
 REM Navigate to the directory where the Python script is located
-cd /d "C:\Users\jcmar\OneDrive\Documents\GitHub\data-analysis\JC Integrator"
+cd /d "C:\path\to\folder\containing\script"
 
 REM Run the Python script with the specified interpreter
 REM Replace "python" with the path to your Python executable if it's not in PATH
