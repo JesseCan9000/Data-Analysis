979bc177282706d3961645bb81c5275dec95bc02
Jesse <canav021@umn.edu>
2024-10-11 16:26:23 -0500

    Added .bat file

---
+++ JC Integrator/JC Integrator.bat
@@ -0,0 +1,10 @@
+@echo off
+REM Navigate to the directory where the Python script is located
+cd /d "C:\Users\jcmar\OneDrive\Documents\GitHub\data-analysis\JC Integrator"
+
+REM Run the Python script with the specified interpreter
+REM Replace "python" with the path to your Python executable if it's not in PATH
+python jc_integrator_gui.py
+
+REM Pause to keep the command prompt window open
+pause
